fd904205e4c2d797f33ee8d4c7ea617afb78cc82
Ed O'Connor <ed@oconnor.app>
2022-10-15 11:33:20 -0400

    navbar edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
- 
+import { Component, OnInit } from '@angular/core';
+
+declare const window: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'oconnor';
+  formModal: any;
+
+  ngOnInit(): void {
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('staticBackdrop')
+    );
+  }
+  openModal() {
+    this.formModal.show();
+  }
+  closeModal() {
+    this.formModal.hide();
+  }
 }
